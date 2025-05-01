39b601e9066f52d11b10fcb29b98dd2ec8eb8141
Alexander Gorbunov <alexandr.gorbunov@smart-steel-technologies.com>
2025-05-01 09:54:14 +0200

    removed-mongo, several info updates

--- internal/storage/projects.go
+++ internal/storage/projects.go
@@ -2,35 +2,15 @@ package storage
 
 import (
 	"alex_gorbunov_cv/internal/models"
-	"context"
-	"fmt"
-
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (storage *Storage) GetProjects() ([]*models.Project, error) {
 	const fn = "storage.GetProjects"
 
-	ctx := context.Background()
-
 	var projects []*models.Project
 
-	cursor, err := storage.collection.Find(ctx, bson.M{"projects": bson.M{"$exists": true}})
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", fn, err)
-	}
-	defer cursor.Close(ctx)
-
-	for cursor.Next(ctx) {
-		var document struct {
-			Projects []*models.Project `bson:"projects"`
-		}
-		err := cursor.Decode(&document)
-		if err != nil {
-			return nil, fmt.Errorf("%s: %w", fn, err)
-		}
-
-		projects = append(projects, document.Projects...)
+	for _, project := range storage.Projects {
+		projects = append(projects, &project)
 	}
 
 	return projects, nil
